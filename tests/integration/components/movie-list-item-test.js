eba31d7d99f52f4bb7e6b5119d7a690bc0f5917e
Hannah Reed <hannahfrancesreed@gmail.com>
2025-01-29 15:44:32 +0000

    movie-list

--- tests/integration/components/movie-list-item-test.js
+++ tests/integration/components/movie-list-item-test.js
@@ -6,11 +6,15 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | MovieListHeader', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('has the correct header text', async function (assert) {
-    this.movie = { title: 'TEST TITLE', description: 'TEST DESCRIPTION' };
-    await render(hbs`<MovieList::MovieListItem />`);
+  test('renders the correct title and description for a movie', async function (assert) {
+    this.movie = {
+      data: () => ({ title: 'TEST TITLE', description: 'TEST DESCRIPTION' }),
+    };
+    await render(hbs`<MovieList::MovieListItem @movie={{this.movie}}/>`);
 
     assert.dom('h2').hasText('TEST TITLE');
     assert.dom('p').hasText('TEST DESCRIPTION');
+    assert.dom('h2').exists();
+    assert.dom('p').exists();
   });
 });
